3ff16e55ad4b436469303be63845ea39bbd5706d
Raymart Domagas <raymartdomagas@Raymarts-MacBook-Air.local>
2023-07-05 21:03:12 -0400

    added code to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
-// Write your Color component here
+// // Write your Color component here
+import React from "react";
+const Color = ({color, setSelectedColor}) => {
+  return <div className={color} onClick={() => setSelectedColor(color)}></div>;
+};
 
 const App = () => {
+  const [selectedColor, setSelectedColor] = React.useState("")
   return (
     <div id="container">
       <div id="navbar">
         <div>Currently selected: </div>
-        <div className="red">red</div>
+        <div className={selectedColor}>{selectedColor}</div>
       </div>
-      <div id="colors-list">{/* colors go here */}</div>
+      <div id="colors-list">
+        <Color color="violet" setSelectedColor={setSelectedColor}/> 
+        <Color color="blue" setSelectedColor={setSelectedColor}/> 
+        <Color color="green"setSelectedColor={setSelectedColor}/>
+      </div>
     </div>
   );
 };
 
 export default App;
+
